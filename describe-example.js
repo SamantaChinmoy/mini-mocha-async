32ddabcc3b6c8ba6a28fe75215c6adc8b33865e1
SamantaChinmoy <chinmoysamanta.2010@gmail.com>
2017-12-07 20:15:53 +0530

    First Commit

---
+++ describe-example.js
@@ -0,0 +1,65 @@
+import assert from 'assert';
+import { test, testSuite, setup, teardown } from './describe';
+
+const obj = {};
+testSuite('True Or False? ', () => {
+  testSuite('setup', () => {
+    test('should setup num', () => {
+      assert.equal(obj.num, 2);
+    });
+    setup(() => {
+      obj.num = 2;
+    });
+    teardown(() => {
+      obj.num = null;
+    });
+  });
+
+  testSuite('teardown', () => {
+    test('should teardown num', () => {
+      assert.equal(obj.num, null);
+    });
+  });
+
+  testSuite('truthy => ', () => {
+    test('empty array', () => {
+      assert.equal(!![0], true);
+    });
+
+    test('empty object', () => {
+      assert.equal(!!{}, true);
+    });
+    test('asyncTest', (done) => {
+      setTimeout(() => {
+        try {
+          assert.equal(1, true);
+          done(); // success case
+        } catch (err) {
+          done(err); // error case
+        }
+      },2000);
+    })
+  });
+
+  testSuite('falsy => ', () => {
+    testSuite('undefined & nil', () => {
+      test('undefined', () => {
+        assert.equal(!(void 0), true);
+      });
+      test('null', () => {
+        assert.equal(!null, true);
+      });
+    });
+
+    test('should test ![] === true ', () => {
+      assert.equal(![], true);
+    });
+
+    test('!NaN === true', () => {
+      assert.equal(!NaN, true);
+    });
+    test('!(empty string) === true', () => {
+      assert.equal(!'', true);
+    });
+  });
+});
---
+++ describe.js
@@ -0,0 +1,67 @@
+const success = desc => console.log(`${desc} : Pass`);
+const failure = (desc, msg) => console.log(`:( ${desc} : Fail => ${msg}`);
+const log = desc => console.log(desc);
+
+const activity = {};
+const stack = [];
+const isEmptyStack = () => stack.length === 0;
+const stackTop = () => stack[stack.length - 1];
+const ctx = {};
+
+const spush = (key, val) => stackTop()[key].push(val);
+
+const indentedTitle = ctxt =>
+    `${stack.map(() => '   ').join('')}${ctxt}`;
+
+const newTop = title =>
+    ({ title, tests: [], setup: [], teardown: [], testSuites: [] });
+
+const execTop = () => 'setup tests testSuites teardown'.split(' ')
+    .forEach(key => stackTop()[key].forEach(activity[key]));
+
+const execTestSuite = (title, testSuiteFn) => {
+    log(indentedTitle(title));
+    stack.push(newTop(title));
+    testSuiteFn.call(ctx); // collect testSuites, setup, teardown and it.
+    execTop(); // execute them
+    stack.pop();
+};
+const reportTests = (fn, title) => {
+    const desc = indentedTitle(title);
+    const done = err => {
+        if (err) {
+            failure(desc, err.message);
+        } else {
+            success(desc);
+        }
+    };
+    if (fn.length) {
+        fn.call(ctx, done);
+    } else {
+        try {
+            fn.call(ctx);
+            success(desc);
+        } catch (e) {
+            failure(desc, e.message);
+        }
+    }
+};
+
+activity.setup = fn => fn.call(ctx);
+activity.teardown = fn => fn.call(ctx);
+activity.testSuites = ([title, testFn]) =>
+    execTestSuite(title, testFn);
+activity.tests = ([title, testFn]) =>
+    reportTests(testFn, title);
+
+export const test = (desc, fn) => spush('tests', [desc, fn]);
+export const testSuite = (title, testfn) => {
+    if (isEmptyStack()) {
+        execTestSuite(title, testfn);
+        return;
+    }
+
+    spush('testSuites', [title, testfn]);
+};
+export const setup = spush.bind(null, 'setup');
+export const teardown = spush.bind(null, 'teardown');
